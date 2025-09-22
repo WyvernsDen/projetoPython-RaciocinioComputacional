d6db4a80b02a166897c6fc573e93092bed68b118
WyvernsDen <danielmariotto06@gmail.com>
2025-09-22 10:19:50 -0300

    implemented "are you sure" for deleting student
    added more comments
    
    fixed formatting

--- main.py
+++ main.py
@@ -56,9 +56,10 @@ select_command = "Informe o comando desejado: "
 def press_enter():
     input("\nPressione `Enter` para continuar.")
 
+# now we're actually getting started
+
 lista_estudantes = []
 
-# defining methods for the menu loops and CRUD
 def main_menu():
     while True:
         main_choice = str.lower(input(select_command))
@@ -118,23 +119,24 @@ def op_menu(main_choice):
             continue
 
 def incluir_estudantes(main_choice):
+    def text_success():
+        print(f"\nEstudante N.º{estudante_codigo} "
+              f"com o nome '{estudante_nome}' e o CPF '{estudante_cpf}' "
+              f"foi incluído(a) com êxito.")
     estudante_codigo = int(input("\nInforme o código do(a) estudante: "))
     estudante_nome = str(input("\nInforme o nome do(a) estudante: "))
     estudante_cpf = str(input("\nInforme o CPF do(a) estudante: "))
-    # first we're checking if the list is empty
     if len(lista_estudantes) == 0:
+    # first we're checking if the list is empty
         estudante_dict = {
             "codigo": estudante_codigo,
             "nome": estudante_nome,
             "cpf": estudante_cpf
         }
         lista_estudantes.append(estudante_dict)
-        print(f"\nEstudante `N.º{estudante_codigo}   "
-              f"Nome: {estudante_nome}   CPF: {estudante_cpf}` "
-              f"foi incluído(a) com êxito.")
-    # if it's not empty we're checking if any dictionary within the list has any of the same info
-    # FIX THIS: creating the methods broke the check for similar students
+        text_success()
     else:
+    # if it's not empty we're checking if any dictionary within the list has any of the same info
         codigo_igual = False
         nome_igual = False
         cpf_igual = False
@@ -148,17 +150,15 @@ def incluir_estudantes(main_choice):
                 cpf_igual = True
             if not codigo_igual and not nome_igual and not cpf_igual:
                 nada_igual = True
-        # if none of the info is the same we're gonna go ahead and create a dictionary for that student
         if nada_igual:
+        # if none of the info is the same we're gonna go ahead and create a dictionary for that student
             estudante_dict = {
                 "codigo": estudante_codigo,
                 "nome": estudante_nome,
                 "cpf": estudante_cpf
             }
             lista_estudantes.append(estudante_dict)
-            print(f"\nEstudante `N.º{estudante_codigo}   "
-                  f"Nome: {estudante_nome}   CPF: {estudante_cpf}` "
-                  f"foi incluído(a) com êxito.")
+            text_success()
         # if any info is repeated we'll tell the user what is
         else:
             if cpf_igual and nome_igual and codigo_igual:
@@ -182,16 +182,18 @@ def incluir_estudantes(main_choice):
             elif codigo_igual and not nome_igual and not cpf_igual:
                 print("\nNão foi possível incluír este(a) estudante, "
                       "já existe um cadastro com o mesmo código.")
-    # someone should probably fix this inconsistency...
+    # SOMEONE should probably fix this inconsistency with press_enter...
     press_enter()
     print(Menu.estudantes)
     op_menu(main_choice)
 
 def listar_estudantes(main_choice):
     if len(lista_estudantes) == 0:
+    # first we're checking if the list is empty
         print("\nComando indisponível: Nenhum(a) estudante foi encontrado(a).")
     else:
-    # FIX THIS: order the list instead of just printing it
+    # if it's not empty we'll print the list
+    # IMPLEMENT: order the list instead of just printing it
         print(f"\nTotal de {len(lista_estudantes)} estudante(s) encontrado(s): ")
         for dicionario_listar in lista_estudantes:
             print(f"N.º{dicionario_listar["codigo"]}   "
@@ -203,23 +205,25 @@ def listar_estudantes(main_choice):
 
 def modificar_estudantes(main_choice):
     if len(lista_estudantes) == 0:
+    # first we're checking if the list is empty
         print("\nComando indisponível: Nenhum(a) estudante foi encontrado(a).")
-        # here's the inconsistency...
+        # here's the press_enter inconsistency...
         press_enter()
         print(Menu.estudantes)
     else:
+    # if it's not empty we're asking the num of the student
         estudante_modificar = None
         codigo_estudante_modificar = input("\nInforme o código do(a) estudante á ser modificado(a): ")
-        # first we're choosing a dictionary to modify
         for dicionario_modificar in lista_estudantes:
+        # then we're finding the right dictionary to modify
             if dicionario_modificar["codigo"] == int(codigo_estudante_modificar):
                 estudante_modificar = dicionario_modificar
                 print(f"\nVocê escolheu o(a) estudante: `N.º{codigo_estudante_modificar}  "
                       f" Nome: {dicionario_modificar["nome"]}   "
                       f"CPF: {dicionario_modificar["cpf"]}`")
                 break
-        # then we're implementing a menu for modifying the chosen dictionary
         while True:
+        # then we're implementing a menu for modifying the chosen dictionary
             if estudante_modificar is None:
                 print(f"\nEstudante N.º{codigo_estudante_modificar} não encontrado(a).")
                 press_enter()
@@ -256,25 +260,39 @@ def modificar_estudantes(main_choice):
                     press_enter()
 
 def excluir_estudantes(main_choice):
+    # first we're checking if the list is empty
     if len(lista_estudantes) == 0:
         print("\nComando indisponível: Nenhum(a) estudante foi encontrado(a).")
     else:
+    # if it's not empty we're asking the code of the student
         estudante_remover = None
         codigo_remover = int(input("\nInforme o código do(a) estudante á ser removido(a): "))
         for dicionario_remover in lista_estudantes:
+        # then we're finding the right dictionary to modify
             if dicionario_remover["codigo"] == codigo_remover:
                 estudante_remover = dicionario_remover
+                estudante_remover_codigo = dicionario_remover["codigo"]
+                estudante_remover_nome = dicionario_remover["nome"]
+                estudante_remover_cpf = dicionario_remover["cpf"]
                 break
         if estudante_remover is None:
             print(f"\nEstudante com o código {codigo_remover} não encontrado(a).")
         else:
-            lista_estudantes.remove(estudante_remover)
+        # now we're going to make sure that the user chose the right student
+            print(f"\nVocê escolheu o(a) estudante: `N.º{estudante_remover_codigo}  "
+                      f" Nome: {estudante_remover_nome}   "
+                      f"CPF: {estudante_remover_cpf}`")
+            option_yn = str.lower(input("\nDigite 'sim' se realmente deseja excluir este(a) estudante: "))
+            if option_yn == 'sim':
+                lista_estudantes.remove(estudante_remover)
+                print(f"\nEstudante N.º{estudante_remover_codigo} "
+                      f"foi excluído(a) com êxito.")
+            else:
+                print("\nEstudante não excluído(a).")
     press_enter()
     print(Menu.estudantes)
     op_menu(main_choice)
 
-
 # prints menu and starts method
 print(Menu.main)
 main_menu()
-# print(Dictionaries.estudantes)
